4176a06e75e7737a02f19d8672afe782abab85e3
jerryissky <jerryissky@gmail.com>
2016-05-09 17:59:43 +0800

    Add Index News

--- Work.WebProj/Controllers/IndexController.cs
+++ Work.WebProj/Controllers/IndexController.cs
@@ -10,8 +10,20 @@ namespace DotWeb.Controllers
     {
         public ActionResult Index()
         {
+            db0 = getDB0();
 
-            return View("Index");
+            var lang = System.Threading.Thread.CurrentThread.CurrentCulture.Name;
+            var items = db0.News.Where(x => x.state == "A" && x.i_Lang == lang)
+                .OrderByDescending(x => x.set_date)
+                .Take(3)
+                .ToList();
+
+            foreach (var item in items)
+            {
+                item.img_list = ImgSrc("Active", "NewsData", item.news_id, "List", "origin");
+            }
+
+            return View("Index", items);
         }
 
         public RedirectResult Login()
