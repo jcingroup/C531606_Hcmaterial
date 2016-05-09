da6ed85933c3d73984ec07e979c14b510d18be04
TinWu <yyuuix@gmail.com>
2016-05-09 17:48:27 +0800

    首頁banner 文字動畫

--- Work.WebProj/app_modules/file1.js
+++ Work.WebProj/app_modules/file1.js
@@ -1,4 +1,5 @@
-"use strict";
+//declare function require(name: string);
+//var sampleModule = require('../modulename/file.jsx');
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
@@ -12,7 +13,7 @@ var Tips = (function (_super) {
     }
     Tips.prototype.render = function () {
         var ABC = JCIN.ContextComponent;
-        return React.createElement("div", null, React.createElement(ABC, {id: 1}));
+        return React.createElement("div", null, React.createElement(ABC, {"id": 1}));
     };
     return Tips;
-}(React.Component));
+})(React.Component);
--- Work.WebProj/app_modules/file2.js
+++ Work.WebProj/app_modules/file2.js
@@ -1,3 +1,4 @@
+/// <reference path="file1.tsx" />
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
@@ -12,4 +13,4 @@ var Tips2 = (function (_super) {
         return React.createElement("div", null);
     };
     return Tips2;
-}(React.Component));
+})(React.Component);
