d75e6a31c06fa15d7db10ef0d60ddee998a7f7ff
TinWu <yyuuix@gmail.com>
2016-07-05 15:01:08 +0800

    簡中+修改教學(後台faq)

---
+++ Work.WebProj/Areas/Base/Controllers/FAQController.cs
@@ -0,0 +1,18 @@
+﻿using DotWeb.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotWeb.Areas.Base.Controllers
+{
+    public class FAQController : AdminController
+    {
+        // GET: Base/FAQ
+        public ActionResult Index()
+        {
+            return View("FAQ");
+        }
+    }
+}
